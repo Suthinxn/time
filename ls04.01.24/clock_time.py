07fc64ad6b023258bbf06a2e6f237160a19ab0f4
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-04 16:29:13 +0700

    add clock

---
+++ ls04.01.24/clock_time.py
@@ -0,0 +1,19 @@
+import time
+
+if __name__ == '__main__':
+    sec = 0
+    minute = 45
+    hour = 12
+    while True :
+        time.sleep(0.1)
+        # print(f" {hour:02d} : {minute:02d} : {sec:02d}")
+        print(f" {hour:02d} : {minute:02d}")
+        sec += 1
+        if sec == 60:
+            sec -= 60
+            minute += 1
+        if minute == 60:
+            hour += 1
+            minute -= 60 
+        if hour == 24 :
+            hour -= 24
