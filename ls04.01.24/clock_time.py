5aa3b603b13ce5bf7f1179a73ce792e6d1050c36
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-04 16:30:21 +0700

    edit clock

--- ls04.01.24/clock_time.py
+++ ls04.01.24/clock_time.py
@@ -2,8 +2,8 @@ import time
 
 if __name__ == '__main__':
     sec = 0
-    minute = 45
-    hour = 12
+    minute = 0
+    hour = 0
     while True :
         time.sleep(0.1)
         # print(f" {hour:02d} : {minute:02d} : {sec:02d}")
